8004d62043d53a8d7861f759e4c555b11346cc9f
515184405 <515184405@qq.com>
2018-04-03 13:08:21 +0800

    1.0.1

---
+++ js/data.js
@@ -0,0 +1,10 @@
+
+var data = [
+	{href : 'http://fy.035k.com',text : '我的博客http://fy.035k.com'},
+	{href : 'http://www.baidu.com',text : '百度'},
+	{href : 'http://www.imooc.com',text : '慕课网'},
+	{href : 'http://www.jq22.com',text : 'jquery插件网'},
+	{href : 'http://www.035k.com',text : '苏打绿可根据历史'},
+	{href : '',text : '死垃圾管理数据管理'},
+	{href : '',text : '另外今年光缆和并购水晶宫老师'}
+]
---
+++ js/index.js
@@ -0,0 +1,79 @@
+
+/**
+
+	姓名：冯宇
+	site: http://fy.035k.com
+	更新时间 ：2018-04-03
+	QQ : 515184405
+	github : https://github.com/515184405/barrage 还有一些其他的项目哦，欢迎star
+
+**/
+
+	$.fn.barrage = function(opt){
+
+		var _self = $(this);
+
+		var opts = {   //默认参数
+			data : [], //数据列表
+			row : 5,   //显示行数
+			time : 2000, //间隔时间
+			gap : 20,    //每一个的间隙
+			position : 'fixed', //绝对定位
+			direction : 'bottom right', //方向
+		}
+
+		var settings = $.extend({},opts,opt); //合并参数
+		var M = {},Obj = {}; 
+		Obj.data = settings.data;
+		M.vertical = settings.direction.split(/\s+/)[0]; //纵向
+		M.horizontal = settings.direction.split(/\s+/)[1]; //横向
+		M.bgColors = ['#edbccc','#edbce7','#c092e4','#9b92e4','#92bae4','#92d9e4','#92e4bc','#a9e492','#d9e492','#e4c892']; //随机背景色数组
+		Obj.arrEle = []; //预计存储dom集合数组
+		M.barrageBox = $('<div id="barrage" style="width:100%;position:'+settings.position+';'+M.vertical+':0;'+M.horizontal+':0;"></div>'); //存所有弹幕的盒子
+		M.timer = null; 
+		var createView = function(){
+			var randomIndex = Math.floor(Math.random() * M.bgColors.length);
+			var ele = $('<a class="overflow-text" target="_blank" style="text-align:'+settings.direction.split(/\s+/)[1]+';float:'+settings.direction.split(/\s+/)[1]+';background-color:'+M.bgColors[randomIndex]+'"; href="'+(Obj.data[0].href ? Obj.data[0].href : "javascript:;")+'">'+Obj.data[0].text+'</a>');
+			var str = Obj.data.shift();
+			if(M.vertical  == 'top'){
+				ele.animate({
+					'opacity' : 1,
+					'margin-top' : settings.gap,
+				},1000)
+				M.barrageBox.prepend(ele);
+			}else{
+				ele.animate({
+					'opacity' : 1,
+					'margin-bottom' : settings.gap,
+				},1000)
+				M.barrageBox.append(ele);
+			}
+			
+			Obj.data.push(str);
+			setTimeout(function(){
+				ele.animate({
+					'opacity' : 0,
+				},300,function(){
+					$(this).css({
+						'margin' : 0,
+					}).remove();
+				})
+			},(settings.time * settings.row))
+		}
+		Obj.close = function(){
+			M.barrageBox.remove();
+			clearInterval(M.timer);
+			M.timer = null;
+		}
+
+		Obj.start = function(){
+			if(M.timer) return;
+			_self.append(M.barrageBox); //把弹幕盒子放到页面中
+			createView(); //创建试图并开始动画
+			M.timer = setInterval(function(){ //循环
+				createView();
+			},settings.time)
+		}
+		return Obj;
+	}
+
